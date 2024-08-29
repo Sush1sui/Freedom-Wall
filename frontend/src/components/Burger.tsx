aef6ba7787b02e514c6fc9a58bcd7f1800ca2d4b
Sush1sui <jplovesraw@gmail.com>
2024-08-29 09:13:48 +0800

    minor change

--- frontend/src/components/Burger.tsx
+++ frontend/src/components/Burger.tsx
@@ -56,7 +56,7 @@ const Burger = () => {
                     }}
                 ></span>
             </button>
-            {isOpen ? (
+            {isOpen && (
                 <nav className="fixed right-4 top-4 z-40 h-[calc(100vh_-_32px)] w-[calc(50%_-_32px)] overflow-hidden font-poppins">
                     <a
                         href="#"
@@ -85,8 +85,6 @@ const Burger = () => {
                         </a>
                     </div>
                 </nav>
-            ) : (
-                ""
             )}
         </div>
     );
